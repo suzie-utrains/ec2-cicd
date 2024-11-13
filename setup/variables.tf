0e8d75dd6be6261fcf2d0d2fb0b14547173ec525
suzie-utrains <98763564+suzie-utrains@users.noreply.github.com>
2024-11-13 13:57:08 +0100

    Create variables.tf

---
+++ setup/variables.tf
@@ -0,0 +1,27 @@
+variable "aws_region" {
+  description = "The AWS region to create resources in"
+  type        = string
+  default     = "us-west-2"
+}
+
+variable "ami_id" {
+  description = "The AMI ID for the instance (Amazon Linux 2 recommended)"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "The type of EC2 instance"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "key_name" {
+  description = "The name of the SSH key pair to use"
+  type        = string
+}
+
+variable "welcome_message" {
+  description = "The welcome message to display on the Apache web server"
+  type        = string
+  default     = "<h1>Welcome to my Apache Web Server</h1>"
+}
